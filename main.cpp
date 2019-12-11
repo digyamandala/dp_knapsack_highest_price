a0b51799ee618fe0f6b8a76ce97662035bb82e3d
Digya Mandala Putra <digyamp@gmail.com>
2019-12-11 11:20:23 +0000

    update code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,29 @@
 
 using namespace std;
 
+int max_price[100][100];
+
+void knapsackTable(int* weights, int*prices, int capacity, int n_things) {
+
+  max_price[0][0] = 0;
+  for(int i = 0 ; i < 100 ; i++) {
+    max_price[i][0] = 0;
+  }
+
+  for(int x = 1 ; x <= capacity ; x++) {
+    for(int k = 1 ; k <= n_things ; k++) {
+      if(x - weights[k] >= 0) {
+        max_price[x][k] = max(max_price[x][k-1], max_price[x-weights[k]][k-1] + prices[k]);
+      }
+      else {
+        max_price[x][k] = max_price[x][k-1];
+      }
+    }
+  }
+}
+
 int max(int a, int b) {
+
   if(a > b) return a;
   return b;
 }
@@ -30,26 +52,11 @@ int main() {
   int capacity = 14;
   int n_things = 5;
 
-  int max_price[100][100];
-  max_price[0][0] = 0;
-
-  for(int i = 0 ; i < 100 ; i++) {
-    max_price[i][0] = 0;
-  }
-
-  for(int x = 1 ; x <= capacity ; x++) {
-    for(int k = 1 ; k <= n_things ; k++) {
-      if(x - weights[k] >= 0) {
-        max_price[x][k] = max(max_price[x][k-1], max_price[x-weights[k]][k-1] + prices[k]);
-      }
-      else {
-        max_price[x][k] = max_price[x][k-1];
-      }
-    }
-  }
+  knapsackTable(weights, prices, capacity, n_things);
 
   for(int i = 0 ; i <= capacity ; i++) {
     cout << "Max profic for bag with " << i << " kg" << ": $" << max_price[i][n_things] << endl;
   }
+  
   return 0;
 }
